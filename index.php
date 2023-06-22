469866cde24f4803374bafdbe229bafc8d938425
DidoucheWasInSti2d <gorimfanboy2002@gmail.com>
2023-06-22 20:53:53 +0200

    Base du Site

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Masterclass en PHP</title>
+</head>
+
+<body>
+    <h1> Bienvnue sur notre Masterclass en PHP</h1>
+    <button><a href="./register.php">S'Inscrire</a></button>
+    <button>Se connecter</button>
+    <button>En savoir plus sur les développeurs de ce super site !</button>
+</body>
+</html>
