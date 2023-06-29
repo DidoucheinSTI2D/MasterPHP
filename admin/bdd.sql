2d2737d4ead9259da7807df5489e90bcaf811c24
DidoucheWasInSti2d <gorimfanboy2002@gmail.com>
2023-06-29 22:26:55 +0200

    Final modif BDD (drop database)

--- admin/bdd.sql
+++ admin/bdd.sql
@@ -1,17 +1,12 @@
-# TODO: DROP DATABASE; pour relancer le script
-# TODO: pas besoin de "`"
+DROP DATABASE `PHPMASTER`;
 CREATE DATABASE `PHPMASTER`;
 USE `PHPMASTER`;
 
-# TODO: "id", "Prenom" soit l'un soit l'autre
 CREATE TABLE `user`(
-    # TODO: INT(11) ??
     `id` INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    # TODO opt: prénom et nom un peu long non?
     `Prenom` varchar(50),
     `Nom` varchar(50),
     `Email` varchar(250),
     `Password` varchar(250),
-    # TODO: imagine tu gère des milliers de roles, 5 caractères suffisent ?
     `role` varchar(5)
 )
